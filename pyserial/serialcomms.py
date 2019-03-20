09819ef535a819ea847b7b38893658b4128f1b7c
Iyalla Alamina (Researcher) <U1273400@pgr.hud.ac.uk>
2019-03-20 17:42:29 +0000

    working serial monitor driver

--- pyserial/serialcomms.py
+++ pyserial/serialcomms.py
@@ -5,7 +5,7 @@
 
 import sys
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import (QApplication, QCheckBox, QGridLayout, QGroupBox,
+from PyQt5.QtWidgets import (QApplication, QCheckBox, QGridLayout, QGroupBox, QPlainTextEdit, 
                              QMenu, QPushButton, QRadioButton, QVBoxLayout, QWidget, QSlider)
 
 class Window(QWidget):
@@ -13,32 +13,44 @@ class Window(QWidget):
         super(Window, self).__init__(parent)
 
         grid = QGridLayout()
-        grid.addWidget(self.createExampleGroup(), 0, 0)
-        grid.addWidget(self.createExampleGroup(), 1, 0)
+        grid.addWidget(self.createSlider(), 0, 0)
+        grid.addWidget(self.createLogArea(), 1, 0)
         #grid.addWidget(self.createExampleGroup(), 0, 1)
         #grid.addWidget(self.createExampleGroup(), 1, 1)
         self.setLayout(grid)
 
-        self.setWindowTitle("PyQt5 Sliders")
+        self.setWindowTitle("STM Terminal")
         self.resize(400, 300)
 
-    def createExampleGroup(self):
-        groupBox = QGroupBox("Slider Example")
+    def createSlider(self):
+        self.groupBox = QGroupBox("DDS Frequency = 1")
 
-        radio1 = QRadioButton("&Radio horizontal slider")
-
-        slider = QSlider(Qt.Horizontal)
-        slider.setFocusPolicy(Qt.StrongFocus)
-        slider.setTickPosition(QSlider.TicksBothSides)
-        slider.setTickInterval(10)
-        slider.setSingleStep(1)
-
-        radio1.setChecked(True)
+        self.slider = QSlider(Qt.Horizontal)
+        self.slider.setFocusPolicy(Qt.StrongFocus)
+        self.slider.setTickPosition(QSlider.TicksBothSides)
+        self.slider.setTickInterval(10)
+        self.slider.setSingleStep(1)
+        self.slider.setMaximum(100)
+        self.slider.setMinimum(1)
+        self.slider.valueChanged.connect(self.sliderChanged)
 
         vbox = QVBoxLayout()
-        vbox.addWidget(radio1)
-        vbox.addWidget(slider)
-        vbox.addStretch(1)
+        vbox.addWidget(self.slider)
+        #vbox.addStretch(1)
+        self.groupBox.setLayout(vbox)
+        return self.groupBox
+
+    def sliderChanged(self):
+        self.groupBox.setTitle("DDS Frequency = "+str(self.slider.value()))
+
+
+    def createLogArea(self):
+        groupBox = QGroupBox("Log")
+
+
+        self.ta = QPlainTextEdit(groupBox)
+        vbox = QVBoxLayout()
+        vbox.addWidget(self.ta)
         groupBox.setLayout(vbox)
 
         return groupBox
