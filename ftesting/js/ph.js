c756acc7e35c61d2bab820e994189aa2955e3f01
John <john.alamina@hud.ac.uk>
2019-04-21 20:48:14 +0100

    superlists2

---
+++ ftesting/js/ph.js
@@ -0,0 +1,11 @@
+// console.log('Hello, world!');
+// phantom.exit();
+
+var page = require('webpage').create();
+page.open('http://example.com', function(status) {
+  console.log("Status: " + status);
+  if(status === "success") {
+    page.render('example.png');
+  }
+  phantom.exit();
+});
